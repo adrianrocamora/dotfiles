54e7886bc650868e73c285a75a9df2baf30feb5c
Adrian Rocamora <adrianrocamorav@gmail.com>
2018-12-12 15:46:06 -0600

    PowerShell colors

---
+++ setColors.ps1
@@ -0,0 +1,7 @@
+$host.PrivateData.ErrorForegroundColor = "DarkGray"
+$host.UI.RawUI.ForegroundColor = "Green"
+$host.UI.RawUI.BackgroundColor = "Black"
+#$host.privatedata
+#$Host
+#Get-Host
+#$Host.UI.RawUI.ForegroundColor = 'white'
