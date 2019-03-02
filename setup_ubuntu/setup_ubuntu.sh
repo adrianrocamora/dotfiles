84a180d5dfb1ca24dc2c0cac8fab9a9b8ded92ef
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-03-02 17:20:30 -0600

    Separated python setup into requirements file

--- setup_ubuntu/setup_ubuntu.sh
+++ setup_ubuntu/setup_ubuntu.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install build-essential -y
@@ -6,31 +7,6 @@ sudo apt install git -y
 sudo apt install curl -y
 sudo apt install clang -y
 sudo apt install python3-pip -y
-python3 -m pip install numpy
-python3 -m pip install scipy
-python3 -m pip install matplotlib
-python3 -m pip install simpy
-python3 -m pip install jupyter
-python3 -m pip install pandas
-python3 -m pip install statsmodels
-python3 -m pip install django
-python3 -m pip install flask
-python3 -m pip install pyqt5
-python3 -m pip install requests
-python3 -m pip install scrapy
-python3 -m pip install pendulum
-python3 -m pip install seaborn
-python3 -m pip install scikit-learn
-python3 -m pip install pyserial
-python3 -m pip install plotly
-python3 -m pip install bokeh
-python3 -m pip install ipython
-python3 -m pip install pyflux
-python3 -m pip install opencv-python
-python3 -m pip install pygame
-python3 -m pip install pillow
-python3 -m pip install paramiko
-python3 -m pip install fabric
 sudo apt install texlive-full -y
 sudo add-apt-repository ppa:neovim-ppa/stable -y
 sudo apt update -y
