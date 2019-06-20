556e7691529c3e167f3153bc1c5c74934e5ea1b1
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-06-19 23:55:37 -0500

    Better vlc song playing

--- play_song.ps1
+++ play_song.ps1
@@ -1,1 +1,2 @@
-D:\prog\VLC\vlc.exe --qt-start-minimized --play-and-exit --qt-notification=0 D:\paradise.mp3
+#D:\prog\VLC\vlc.exe --qt-start-minimized --play-and-exit --qt-notification=0 D:\paradise.mp3
+D:\prog\VLC\vlc.exe  --play-and-exit --qt-notification=0 D:\paradise.mp3
