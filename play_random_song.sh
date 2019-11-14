5983983eb67cbe736af8b5a94fbcf5ab6bade835
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-11-14 10:51:10 -0600

    Updated play random song script for termux

--- play_random_song.sh
+++ play_random_song.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
-find /media/rocamora/datawin/del/music/Japanese\ Music/ -type f -name "*.mp3" | shuf -n 1 | xargs -I {} play {}
+find /storage/0123-4567/Music/jp -type f -name "*.mp3" | shuf -n 1 | xargs -I {} play {}
+#find /media/rocamora/datawin/del/music/Japanese\ Music/ -type f -name "*.mp3" | shuf -n 1 | xargs -I {} play {}
