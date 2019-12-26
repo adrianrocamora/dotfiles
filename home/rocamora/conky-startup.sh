e304d5b68fe8b07f3200f305f6d65d4fbfb8b758
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-12-25 21:30:13 -0600

    Importing conky config

---
+++ home/rocamora/conky-startup.sh
@@ -0,0 +1,6 @@
+sleep 20s
+killall conky
+cd "/home/rocamora/.conky/Green Apple Desktop"
+conky -c "/home/rocamora/.conky/Green Apple Desktop/Gotham" &
+cd "/home/rocamora/.conky/Green Apple Desktop"
+conky -c "/home/rocamora/.conky/Green Apple Desktop/conky_seamod" &
