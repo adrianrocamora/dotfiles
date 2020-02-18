f6cad8a3020f41356739af23e0890a54e84880df
Adrian Rocamora <adrianrocamorav@gmail.com>
2020-02-18 10:43:06 -0600

    Updates

--- home/rocamora/util/play_random_song.sh
+++ home/rocamora/util/play_random_song.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 #find /storage/0123-4567/Music/jp -type f -name "*.mp3" | shuf -n 1 | xargs -I {} play {}
+# On arch linux requires sox
 find ~/docs/music/ -type f -name "*.mp3" | shuf -n 1 | xargs -I {} play {}
